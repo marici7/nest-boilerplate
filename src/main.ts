a766d3a31620870f12fdd340f66243bf6e3a16fa
Mehmet ARICI <mehmetaricistd@gmail.com>
2023-12-09 01:05:47 +0300

    env config and file added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as process from 'process';
 
 import { AppModule } from './app.module';
 
+const { PORT } = process.env;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(PORT || 8080);
 }
 bootstrap();
